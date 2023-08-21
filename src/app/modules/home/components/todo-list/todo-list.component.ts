414aab296a15dcbb660362f3ac5b4f5e46257680
Elvis Claudino <102040112+elvisclaudino@users.noreply.github.com>
2023-08-21 15:26:31 -0300

    [add]: show items

--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+// Interface
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
+  public taskList: Array<TaskList> = [];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
